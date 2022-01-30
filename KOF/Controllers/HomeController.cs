3b3407eef03d925e7cc61dca1023ebe1d91ceafe
toqeerzia123 <41077688+toqeerzia123@users.noreply.github.com>
2022-01-30 15:08:37 +0500

    Add to Cart
    Add to Cart

--- KOF/Controllers/HomeController.cs
+++ KOF/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using KOF.Models;
+using KOF.Services.InventoryService;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,12 +14,69 @@ namespace KOF.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IInventoryService _inventoryService;
+      
+        public HomeController(ILogger<HomeController> logger, IInventoryService inventoryService)
         {
             _logger = logger;
+            _inventoryService = inventoryService;
         }
 
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult ProductListView()
+        {
+            return View();
+        }
+        public async Task<IActionResult> ProductGridView(int id)
+        {
+            //var data = await _inventoryService.CategorywiseInventory(id);
+            //ViewBag.Categorywiseproducts = data;
+            ViewBag.id = id;
+                return View();
+        }
+        public async Task<IActionResult> Productdetail(int id)
+        {
+            //var data = await _inventoryService.CategorywiseInventory(id);
+            //ViewBag.Categorywiseproducts = data;
+            ViewBag.InventoryId = id;
+            return View();
+        }
+
+
+        public IActionResult About()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Addtocard(int inventoryid,int productid,int Qty,int userid,string unit)
+        {
+            var data = await _inventoryService.addtocart( inventoryid,  productid,  Qty,  userid, unit);
+            return RedirectToAction("Productdetail",new {id=productid });
+        }
+        public IActionResult MyAccount()
+        {
+            return View();
+        }
+        public IActionResult  removecart(int id)
+        {
+       
+            _inventoryService.removecart(id);
+            return RedirectToAction("index");
+   
+        }
+        public IActionResult WishList()
+        {
+            return View();
+        }
+        public IActionResult Checkout()
         {
             return View();
         }
